6ecb183d73bba0076abec9f8dc52185e42a2eb2e
BIT Student <classroom@bgit.rs>
2019-06-10 17:24:14 +0200

    prvi react

---
+++ react/app.js
@@ -0,0 +1,25 @@
+const Beertitle = (beer) => {
+
+    const a = asdad
+    return (
+        <h2 id={a} > {beer}</h2>
+    )
+
+}
+
+
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <p>
+                neki tekst
+          </p>
+        </div>
+
+    )
+}
+ReactDOM.render(<App />, document.querySelector('.root'))
