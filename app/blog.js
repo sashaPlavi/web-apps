35e639f1e3e8312d2c823ea42e3e8e3ef641c694
BIT Student <classroom@bgit.rs>
2019-06-11 16:34:33 +0200

    novi react

--- app/blog.js
+++ app/blog.js
@@ -1,59 +1,341 @@
+
+
+
 const Header = (props) => {
     return (<React.Fragment>
-        <h3></h3>
+        <div className="postHeader">
+            <h3>{props.text}</h3>
+        </div>
     </React.Fragment>
     )
 }
 
 const UserInfo = (props) => {
-    return (<React.Fragment>
-        <img src={props.img} />
-        <p>{props.name}</p>
-        <p>{props.email}</p>
-        <p>{props.dateOfBirth}</p>
-    </React.Fragment>
+
+    return (
+        <div className="userInfo">
+            <img src={props.element.picture.medium} className="photo" />
+            <div className="userText">
+                <p> name:{props.element.name.first} {props.element.name.last}</p>
+                <p> email:{props.element.email}</p>
+                <p> age:{props.element.dob.age}</p>
+
+                <p> ctiy:{props.element.location.city}</p>
+                <p> gender:{props.element.gender}</p>
+            </div>
+            <hr />
+        </div>
     )
+
 }
 
 const UserList = (props) => {
     return (
         <div>
-            {props.users.map(user => <UserInfo user={user} />)}
+            {props.user.map((res, i) => <UserInfo element={res} key={i} />)}
         </div>
     )
 }
 
 
-const Main = () => {
+const Main = (props) => {
+
     return (
-        <main>
-            <UserList users={props.data} />
+        <main className="body">
+            <UserList user={props.users} />
         </main>
     )
 }
 
 const Footer = () => {
-    const time = new Date().getFullYear;
+    const time = new Date().getFullYear();
+    console.log(time);
+
     return (
 
-        <footer>
-            <p>&copy{time}</p>
+        <footer className="footer">
+            <p>&copy;{time}</p>
         </footer>
     )
 }
-const UserListData = [
+const UserListData =
+{
+    'results': [
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "petros",
+                "last": "bitzer"
+            },
+            "location": {
+                "street": "lindenstraße 187",
+                "city": "isselburg",
+                "state": "mecklenburg-vorpommern",
+                "postcode": 33605,
+                "coordinates": {
+                    "latitude": "-37.8740",
+                    "longitude": "-18.9553"
+                },
+                "timezone": {
+                    "offset": "+6:00",
+                    "description": "Almaty, Dhaka, Colombo"
+                }
+            },
+            "email": "petros.bitzer@example.com",
+            "login": {
+                "uuid": "c0cc9674-bb8d-49af-b5e1-a2c3b375280f",
+                "username": "tinybird686",
+                "password": "franco",
+                "salt": "UTFZKP7S",
+                "md5": "9b79000542f2d49b1be9503cf4c7d00d",
+                "sha1": "19d7a70655093aebffdc593376843fad5c2a1a89",
+                "sha256": "400725a81e2f2ec3ce0eafe61b3e67d0e60dc43026f35e482f8ae33680b85f43"
+            },
+            "dob": {
+                "date": "1954-05-09T06:36:09Z",
+                "age": 65
+            },
+            "registered": {
+                "date": "2014-11-11T01:17:23Z",
+                "age": 4
+            },
+            "phone": "0724-3150940",
+            "cell": "0177-1653372",
+            "id": {
+                "name": "",
+                "value": null
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/20.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/20.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/20.jpg"
+            },
+            "nat": "DE"
+        },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "arturo",
+                "last": "ibañez"
+            },
+            "location": {
+                "street": "8598 calle nebrija",
+                "city": "vitoria",
+                "state": "cantabria",
+                "postcode": 19191,
+                "coordinates": {
+                    "latitude": "-4.7987",
+                    "longitude": "-151.0872"
+                },
+                "timezone": {
+                    "offset": "+3:30",
+                    "description": "Tehran"
+                }
+            },
+            "email": "arturo.ibañez@example.com",
+            "login": {
+                "uuid": "c29855c3-e572-469d-af31-5c430a65dd4d",
+                "username": "tinyelephant852",
+                "password": "smokey",
+                "salt": "tWqndPzv",
+                "md5": "4c771041d94a39ad2858347444aad5f9",
+                "sha1": "7e371bf66a77e0d47f03e84d6147f1cc9db48067",
+                "sha256": "ce54338f11afdead2549d5416105ce61f8c5c561183d9e8bb61f7452823cbb57"
+            },
+            "dob": {
+                "date": "1954-11-06T12:22:31Z",
+                "age": 64
+            },
+            "registered": {
+                "date": "2005-05-18T17:48:08Z",
+                "age": 14
+            },
+            "phone": "945-959-276",
+            "cell": "606-294-358",
+            "id": {
+                "name": "DNI",
+                "value": "40735773-S"
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/55.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/55.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/55.jpg"
+            },
+            "nat": "ES"
+        },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "deniz",
+                "last": "koçyiğit"
+            },
+            "location": {
+                "street": "668 doktorlar cd",
+                "city": "kahramanmaraş",
+                "state": "karabük",
+                "postcode": 61780,
+                "coordinates": {
+                    "latitude": "-55.2488",
+                    "longitude": "-109.9305"
+                },
+                "timezone": {
+                    "offset": "+10:00",
+                    "description": "Eastern Australia, Guam, Vladivostok"
+                }
+            },
+            "email": "deniz.koçyiğit@example.com",
+            "login": {
+                "uuid": "869f9e3c-90a4-4e76-9d70-b48bc4be238f",
+                "username": "brownzebra970",
+                "password": "othello",
+                "salt": "YOoMqfBs",
+                "md5": "200ddb3a575860c4882a14ea21dc7fcf",
+                "sha1": "262e157c3a4b0be58fd3ced7977a5ee2f5f72210",
+                "sha256": "10ec3a61d8756a0902408ca3e07badaed2e8ce080b1488a0792fd850a8ac3d74"
+            },
+            "dob": {
+                "date": "1972-03-24T23:15:37Z",
+                "age": 47
+            },
+            "registered": {
+                "date": "2012-05-02T23:57:28Z",
+                "age": 7
+            },
+            "phone": "(942)-972-6256",
+            "cell": "(554)-683-1796",
+            "id": {
+                "name": "",
+                "value": null
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/44.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/44.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/44.jpg"
+            },
+            "nat": "TR"
+        },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "brad",
+                "last": "newman"
+            },
+            "location": {
+                "street": "3273 springfield road",
+                "city": "edenderry",
+                "state": "kildare",
+                "postcode": 38439,
+                "coordinates": {
+                    "latitude": "-77.9791",
+                    "longitude": "-132.5350"
+                },
+                "timezone": {
+                    "offset": "+6:00",
+                    "description": "Almaty, Dhaka, Colombo"
+                }
+            },
+            "email": "brad.newman@example.com",
+            "login": {
+                "uuid": "9f9da394-174a-43ff-897f-74fb4a489897",
+                "username": "lazydog677",
+                "password": "lala",
+                "salt": "tfsjeKeC",
+                "md5": "06840ebb88b32cff2784c892bd03918e",
+                "sha1": "1de0ab082cae1324b215a6ddcaf42d63105990a7",
+                "sha256": "a1242c908a15a7b66b820e56a54e400bfbb2a2620cddec1c593c63d4e9fa493b"
+            },
+            "dob": {
+                "date": "1973-12-01T17:18:31Z",
+                "age": 45
+            },
+            "registered": {
+                "date": "2011-11-11T12:23:38Z",
+                "age": 7
+            },
+            "phone": "021-076-9165",
+            "cell": "081-050-8608",
+            "id": {
+                "name": "PPS",
+                "value": "6848928T"
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/31.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/31.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/31.jpg"
+            },
+            "nat": "IE"
+        },
+        {
+            "gender": "female",
+            "name": {
+                "title": "miss",
+                "first": "آوین",
+                "last": "سالاری"
+            },
+            "location": {
+                "street": "7369 آیت الله سعیدی",
+                "city": "دزفول",
+                "state": "یزد",
+                "postcode": 78477,
+                "coordinates": {
+                    "latitude": "30.2363",
+                    "longitude": "133.4235"
+                },
+                "timezone": {
+                    "offset": "+10:00",
+                    "description": "Eastern Australia, Guam, Vladivostok"
+                }
+            },
+            "email": "آوین.سالاری@example.com",
+            "login": {
+                "uuid": "51bfa911-ac5c-4665-8142-1917882a8952",
+                "username": "bigpanda836",
+                "password": "katie",
+                "salt": "BUBY54Ix",
+                "md5": "0ccf5bdd303f8bf1e01b8168fe1f72fc",
+                "sha1": "59856c36521aeb9040029bffe1ce5478457cf8af",
+                "sha256": "508abc0fd483c7ddd60ca4bc97257debe8bc63ee61878dc8745d616b07bfa4b9"
+            },
+            "dob": {
+                "date": "1978-09-28T21:13:41Z",
+                "age": 40
+            },
+            "registered": {
+                "date": "2002-05-20T08:51:18Z",
+                "age": 17
+            },
+            "phone": "020-35220027",
+            "cell": "0949-597-2710",
+            "id": {
+                "name": "",
+                "value": null
+            },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/81.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/81.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/81.jpg"
+            },
+            "nat": "IR"
+        },
+
+    ]
+}
 
 
-]
-
-const myAppp = () => {
+const MyAppp = () => {
     return (
         <React.Fragment>
 
-            <Header />
-            <Main user={} />
+            <Header text='React Users' />
+            <Main users={UserListData.results} />
             <Footer />
         </React.Fragment>
 
     )
 }
+
+ReactDOM.render(<MyAppp />, document.querySelector('.root'))
