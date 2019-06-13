6237a31aa3a740e197edb1ebf27a0fa0c95de575
BIT Student <classroom@bgit.rs>
2019-06-13 14:52:45 +0200

    malo stilizovano

--- bitPeople/bit-people/src/view/App.js
+++ bitPeople/bit-people/src/view/App.js
@@ -1,43 +1,43 @@
 import React from 'react';
 
- 
+import './style.css'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { Main } from './Main'
 import { fetchUsers } from './sevices/userService'
 
-class  MyAppp extends React.Component  {
-    constructor(props){
-        super (props);
-        this.state = {
-          users:[]
-        };
-    }
+class MyAppp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
 
-    componentDidMount() {
-        fetchUsers()
-          .then(users => this.setState({ users }))
-          
-        }
-        
-        
-        
-        
-   render(){
+  componentDidMount() {
+    fetchUsers()
+      .then(users => this.setState({ users }))
 
-       console.log(this.state.users);
-       
-       return (
-           
-           <React.Fragment>
+  }
 
-            <Header text='React Users' />
-            <Main users={this.state.users} />
-            <Footer />
-        </React.Fragment>
 
-)
+
+
+  render() {
+
+    console.log(this.state.users);
+
+    return (
+
+      <React.Fragment>
+
+        <Header text='React Users' />
+        <Main users={this.state.users} />
+        <Footer />
+      </React.Fragment>
+
+    )
+  }
 }
-}  
 
 export { MyAppp }
--- bitPeople/bit-people/src/view/sevices/userService.js
+++ bitPeople/bit-people/src/view/sevices/userService.js
@@ -1,26 +1,27 @@
- import {MyUser} from '../enites/myUser'
+import { MyUser } from '../enites/myUser'
+import axios from 'axios'
 
 export const fetchUsers = () => {
-     
-    return fetch("https://randomuser.me/api/?results=50")
-        .then(response => response.json())
-        .then(users =>users.results.map(user =>{
+
+    return axios.get("https://randomuser.me/api/?results=50")
+        .then(users => users.data.results.map(user => {
 
             const name = user.name.first;
             const gender = user.gender;
             const id = user.id.name;
             const img = user.picture.medium;
-            
-            const myuser = new MyUser ( name, gender, id, img )
-             
+
+            const myuser = new MyUser(name, gender, id, img)
+
             return myuser
         })
 
 
-        
-        )}
-            
- 
-        
-         
+
+        )
+}
+
+
+
+
 
